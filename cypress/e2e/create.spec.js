fd6a3ffb4b22fd74ec465470d0e5e9de302d8fb5
Gsuru <guillermo.suruguay@gmail.com>
2025-07-22 12:04:58 -0300

    Se agregan casos negativos a GET create

--- cypress/e2e/create.spec.js
+++ cypress/e2e/create.spec.js
@@ -2,7 +2,7 @@ import { createPet } from '../api/pet/createPet';
 import { generatePet } from '../data/petFactory';
 
 describe('POST /pet', () => {
-  it('debe crear un pet correctamente', () => {
+  it('Caso positivo - Creación de un pet correctamente', () => {
     const pet = generatePet();
 
     createPet(pet).then((res) => {
@@ -14,6 +14,34 @@ describe('POST /pet', () => {
       });
       expect(res.body).to.have.property('photoUrls');
       expect(res.headers).to.have.property('content-type').and.include('application/json');
+    });
+  });
+
+  it('Caso negativo - Creación de un pet sin campo obligatorio name', () => {
+    const invalidPet = {
+      id: Date.now(),
+      photoUrls: [],
+      tags: [],
+      status: 'available'
+    };
+
+    return createPet(invalidPet, { failOnStatusCode: false }).then((res) => {
+      expect(res.status).to.eq(405);
+      expect(res.body).to.have.property('message');
+    });
+  });
+
+  it('Caso negativo - Creación de un pet con valor status inválido', () => {
+    const invalidPet = {
+      id: Date.now(),
+      photoUrls: [],
+      tags: [],
+      status: 'inválido'
+    };
+
+    return createPet(invalidPet, { failOnStatusCode: false }).then((res) => {
+      expect(res.status).to.eq(405);
+      expect(res.body).to.have.property('message');
     });
   });
 });
