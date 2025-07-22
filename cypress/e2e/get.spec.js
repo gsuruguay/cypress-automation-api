2799a94a323457057141626f5b6e777c4d36bf95
Gsuru <guillermo.suruguay@gmail.com>
2025-07-22 12:13:29 -0300

    Se diferencian tipos de casos

--- cypress/e2e/get.spec.js
+++ cypress/e2e/get.spec.js
@@ -3,17 +3,14 @@ import { waitForPet }    from '../api/pet/getPet';
 import { generatePet }   from '../data/petFactory';
 
 describe('GET /pet/{id}', () => {
-  it('debe devolver un pet existente', () => {
+  it('Caso positivo - Búsqueda de un pet existente', () => {
     const pet = generatePet();
 
-    // 1) Crear el pet
     createPet(pet).then((createRes) => {
       expect(createRes.status).to.eq(200);
 
-      // 2) Esperar activamente a que el pet responda con 200
       return waitForPet(pet.id, { retries: 10, delay: 1000 });
     }).then((res) => {
-      // 3) Validar la respuesta final
       expect(res.status).to.eq(200);
       expect(res.body).to.include({
         id: pet.id,
@@ -24,7 +21,7 @@ describe('GET /pet/{id}', () => {
     });
   });
 
-  it('debe retornar 404 si no existe el pet', () => {
+  it('Caso negativo - Búsqueda de un pet con ID inexistente', () => {
     const fakeId = 13906;
 
     cy.request({
