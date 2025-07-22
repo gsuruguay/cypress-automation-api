3d6986d950aa8cf6a58d7216e58f9d29f1190a2e
Gsuru <guillermo.suruguay@gmail.com>
2025-07-22 15:10:41 -0300

    Se agrega caso negativo para GET find

--- cypress/e2e/find.spec.js
+++ cypress/e2e/find.spec.js
@@ -1,13 +1,19 @@
-import { findByStatus, findByTags } from '../api/pet/getPet';
+import { findByStatus } from '../api/pet/getPet';
 
 describe('GET /pet/findByStatus y findByTags', () => {
-  it('B´squeda de pets por status available', () => {
+  it('Caso positivo - Búsqueda de pets por status available', () => {
     return findByStatus('available').then((res) => {
       expect(res.status).to.eq(200);
       expect(res.body).to.be.an('array');
       res.body.forEach(pet => {
         expect(pet).to.have.property('status', 'available');
       });
+    });
+  });
+
+  it('Caso negativo - Búsqueda de pets por status inválido', () => {
+    return findByStatus('incorrecto').then((res) => {
+      expect(res.status).to.eq(400);
     });
   });
 });
