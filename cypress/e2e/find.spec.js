da795781f2d44c5ad44edc0df96622945463ad83
Gsuru <guillermo.suruguay@gmail.com>
2025-07-22 11:49:37 -0300

    Caso positivo GET para búsqueda por status

---
+++ cypress/e2e/find.spec.js
@@ -0,0 +1,13 @@
+import { findByStatus, findByTags } from '../api/pet/getPet';
+
+describe('GET /pet/findByStatus y findByTags', () => {
+  it('B´squeda de pets por status available', () => {
+    findByStatus('available').then((res) => {
+      expect(res.status).to.eq(200);
+      expect(res.body).to.be.an('array');
+      res.body.forEach(pet => {
+        expect(pet).to.have.property('status', 'available');
+      });
+    });
+  });
+});
